eebfdb8c08f40fff3cacd616b0bd5f3ae6d8fa3b
SeYeol00 <dmot0126@gmail.com>
2023-03-14 15:16:13 +0900

    깃 커밋 체크

--- src/main/java/memory/MemoryAutoConfig.java
+++ src/main/java/memory/MemoryAutoConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 @Slf4j
-@AutoConfiguration // 라이브러리 자동 구성 컨피규레이션
+@AutoConfiguration // 라이브러리 자동 구성 컨피규레이션 ////
 @ConditionalOnProperty(name = "memory", havingValue = "on") // 어노테이션 if문
 public class MemoryAutoConfig {
 
