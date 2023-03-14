4dcf8a67fdeff915c88d1dd2f05fbcae6403b2d4
SeYeol00 <dmot0126@gmail.com>
2023-03-14 15:13:07 +0900

    자동구성 라이브러리 버전 업

---
+++ src/main/java/memory/MemoryAutoConfig.java
@@ -0,0 +1,28 @@
+package memory;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+@AutoConfiguration // 라이브러리 자동 구성 컨피규레이션
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 어노테이션 if문
+public class MemoryAutoConfig {
+
+    @Bean
+    public MemoryFinder memoryFinder(){
+        return new MemoryFinder();
+    }
+
+    @Bean
+    public MemoryController memoryController(){
+        return new MemoryController(memoryFinder());
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("MemoryAutoConfig Init");
+    }
+}
